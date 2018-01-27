eea02f66578091e7e65ac0481448e156edc79ead
jeremyfa <contact@jeremyfa.com>
2018-01-27 17:57:55 +0100

    Prepare 0.3.3 release

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "${0%/*}"
+
+rm -rf sample/ios/bin
+rm -rf sample/ios/project.ios/build
+rm -rf sample/ios/project.ios/IosSample.xcodeproj/project.xcworkspace/xcuserdata
+rm -rf sample/ios/project.ios/IosSample.xcodeproj/xcuserdata
